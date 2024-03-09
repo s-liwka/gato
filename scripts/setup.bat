bb655b7cc2f6e17fc7e6851a01327b2642dbc7c2
sliwka <sliwka.googel@gmail.com>
2024-03-09 17:48:45 +0100

    should work now

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -4,14 +4,12 @@ setlocal
 cd "C:\Program Files"
 git clone https://github.com/s-liwka/gato.git
 
-python -m venv .venv
-call ".venv\Scripts\activate.bat"
+python -m venv "C:\Program Files\gato\.venv"
+call "C:\Program Files\gato\.venv\Scripts\activate.bat"
 
 pip install -r "C:\Program Files\gato\requirements.txt"
 
-set SCRIPT="C:\Program Files\gato\scripts\gato-gui.bat"
-set SHORTCUT="%USERPROFILE%\Desktop\Gato.lnk"
-cscript //nologo "C:\Program Files\gato\scripts\create-shortcut.vbs" "%SHORTCUT%" "%SCRIPT%" "C:\Program Files\gato" "C:\Program Files\gato\resources\gato_pelon.ico"
+cscript //nologo "C:\Program Files\gato\scripts\create-shortcut.vbs" "%USERPROFILE%\Desktop\Gato.lnk" "C:\Program Files\gato\scripts\gato-gui.bat" "C:\Program Files\gato" "C:\Program Files\gato\resources\gato_pelon.ico"
 
 echo ####################################
 echo #      GATO SETUP SUCCESSFUL       #
