bc04b3799150226158b3287289434dc4ccacd93e
sliwka <sliwka.googel@gmail.com>
2024-03-11 23:03:39 +0100

    fix incorrect cwd

--- scripts/gato-cli.sh
+++ scripts/gato-cli.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /usr/local/bin/gato/.venv/bin/activate
-python /usr/local/bin/gato/cli.py "$@"
+cd /usr/local/bin/gato
+python /usr/local/bin/gato/cli.py "$0"
--- scripts/gato-gui.sh
+++ scripts/gato-gui.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source /usr/local/bin/gato/.venv/bin/activate
+cd /usr/local/bin/gato
 python /usr/local/bin/gato/main.py "$@"
